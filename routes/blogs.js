9a14511d912737b108db78b89c6f8f7b687ac468
Kanhav Mathur <kanhavmathur05@gmail.com>
2020-06-28 01:46:21 +0530

    GET POST request with mongodb connection in express application

--- routes/blogs.js
+++ routes/blogs.js
@@ -5,23 +5,31 @@ var MongoClient = require('mongodb').MongoClient
 router.post('/savepost',function(req,res,next)
 {
   console.log('sdfjksdkjfsdkfnksd.sdfsdf',req.body)   
-  var x;     
   MongoClient.connect('mongodb://localhost:27017/blogitdb',{ useUnifiedTopology: true }, function (err, client) {
     if (err) throw err
      
     var db = client.db('blogitdb')
 
 
-    db.collection('users').find().toArray(function (err, result) {
-      if (err) throw err
-      console.log(result)
+    db.collection('users').insertOne(req.body.data, function(err, result){
+      if(err) throw err;
+      console.log("1 Document Inserted");
+
     })
-    // db.collection('users').insertOne(req.body, function(err,result){
+
+    // db.collection('users').find().toArray(function (err, result) {
     //   if (err) throw err
-    //   console.log(err)
+    //   console.log(result)
     // })
   })
-  res.send(this.x)
+  res.send("Hit Occured")
+});
+
+router.get("/getuser/:userId/:newid",function(req,res,next)
+{
+  // console.log('this.reqparam->',req.params.newid)
+  // res.send(req.params.newid);
+  // console.log();
 });
 
 module.exports = router;
